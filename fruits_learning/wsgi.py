957d00395689bef6037ca2bc48fcbd37774f0177
Chen Zhe <fruitschen@gmail.com>
2017-03-26 10:28:32 +0800

    enable whitenoise on the server

--- fruits_learning/wsgi.py
+++ fruits_learning/wsgi.py
@@ -12,7 +12,9 @@ from __future__ import absolute_import, unicode_literals
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fruits_learning.settings.dev")
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root='/home/fruitschen/srv/frutis_learning/static')
