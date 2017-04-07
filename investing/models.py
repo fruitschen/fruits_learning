b1e106b204680875640be8f5e723ea11481504f9
Chen Zhe <fruitschen@gmail.com>
2017-04-07 15:35:04 +0800

    change blogposts sort order

--- investing/models.py
+++ investing/models.py
@@ -15,7 +15,7 @@ class BlogIndexPage(Page):
     def get_context(self, request):
         # Update context to include only published posts, ordered by reverse-chron
         context = super(BlogIndexPage, self).get_context(request)
-        blogpages = self.get_children().live().order_by('-first_published_at')
+        blogpages = self.get_children().live().order_by('first_published_at')
         context['blogpages'] = blogpages
         return context
 
