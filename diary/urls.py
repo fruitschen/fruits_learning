be1fc4e55bedbfe1c04984df72e744f0e6e52d10
Chen Zhe <fruitschen@gmail.com>
2017-09-08 18:06:28 +0800

    switch to class based views

--- diary/urls.py
+++ diary/urls.py
@@ -3,13 +3,13 @@ from django.conf.urls import *
 from diary.views import *
 
 urlpatterns = [
-    url(r'^$', diary_index, name='diary_index'),
-    url(r'^list/$', diary_list, name='diary_list'),
-    url(r'^events/$', diary_events, name='diary_events'),
-    url(r'^todo/$', diary_todo, name='diary_todo'),
+    url(r'^$', DiaryIndex.as_view(), name='diary_index'),
+    url(r'^list/$', DiaryList.as_view(), name='diary_list'),
+    url(r'^events/$', DiaryEvents.as_view(), name='diary_events'),
+    url(r'^todo/$', DiaryTodo.as_view(), name='diary_todo'),
     url(r'^details/(?P<diary_date>\d{4}-\d{2}-\d{2})/$', diary_details, name='diary_details'),
-    url(r'^details/(?P<diary_id>\d+)/add_text/$', diary_add_text, name='diary_add_text'),
-    url(r'^details/(?P<diary_id>\d+)/add_image/$', diary_add_image, name='diary_add_image'),
-    url(r'^edit_text/(?P<content_id>\d+)/$', diary_edit_text, name='diary_edit_text'),
-    url(r'^update_task/$', update_task, name='update_task'),
+    url(r'^details/(?P<diary_id>\d+)/add_text/$', DiaryAddText.as_view(), name='diary_add_text'),
+    url(r'^details/(?P<diary_id>\d+)/add_image/$', DiaryAddImage.as_view(), name='diary_add_image'),
+    url(r'^edit_text/(?P<content_id>\d+)/$', DiaryEditText.as_view(), name='diary_edit_text'),
+    url(r'^update_task/$', UpdateTaskView.as_view(), name='update_task'),
 ]
--- diary/views.py
+++ diary/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.admin.views.decorators import staff_member_required
 from django.utils import timezone
+from django.utils.decorators import method_decorator
+from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.reverse import reverse
 
@@ -24,75 +26,93 @@ def base_diary_context():
         'tomorrow': tomorrow.strftime(DATE_FORMAT),
     }
 
-@staff_member_required
-def diary_index(request):
-    today = date.today()
-    return diary_details(request, today.strftime(DATE_FORMAT))
+
+class DiaryIndex(View):
+    @method_decorator(staff_member_required)
+    def get(self, request):
+        today = date.today()
+        return diary_details(request, today.strftime(DATE_FORMAT))
 
 
-@staff_member_required
-def diary_list(request):
-    diary_items = Diary.objects.all().order_by('-date')
-    context = {
-        'hide_header_footer': True,
-        'diary_items': diary_items,
-    }
-    context.update(base_diary_context())
-    return render(request, 'diary/diary_list.html', context)
+class DiaryList(View):
+    @method_decorator(staff_member_required)
+    def get(self, request):
+        diary_items = Diary.objects.all().order_by('-date')
+        context = {
+            'hide_header_footer': True,
+            'diary_items': diary_items,
+        }
+        context.update(base_diary_context())
+        return render(request, 'diary/diary_list.html', context)
 
 
-@staff_member_required
-def diary_details(request, diary_date):
-    diary_date = datetime.strptime(diary_date, DATE_FORMAT).date()
-    diary_query = Diary.objects.filter(date=diary_date)
-    commit = True
-    today = date.today()
-    if diary_query.exists():
-        diary = diary_query[0]
-    elif diary_date <= today:
-        diary = Diary.objects.create(date=diary_date)
-    elif diary_date > today:
-        diary = Diary(date=diary_date)
-        commit = False
-    events = get_events_by_date(diary.date, commit=commit)
+class DiaryDetails(View):
+    @method_decorator(staff_member_required)
+    def get(self, request, diary_date):
+        diary_date = datetime.strptime(diary_date, DATE_FORMAT).date()
+        diary_query = Diary.objects.filter(date=diary_date)
+        commit = True
+        today = date.today()
+        if diary_query.exists():
+            diary = diary_query[0]
+        elif diary_date <= today:
+            diary = Diary.objects.create(date=diary_date)
+        elif diary_date > today:
+            diary = Diary(date=diary_date)
+            commit = False
+        events = get_events_by_date(diary.date, commit=commit)
 
-    tasks = filter(lambda event: event.is_task, events)
-    tasks_all_done = not filter(lambda task: not task.is_done,tasks)
-    now = timezone.now()
-    hidden_events_count = 0
-    for event in events:
-        if tasks_all_done and getattr(event, 'is_done', False):
-            event.hidden = True
-        if today == diary_date and event.end_hour:
-            if now > timezone.datetime(
-                    now.year, now.month, now.day,
-                    int(event.end_hour),
-                    int(event.end_min or 0),
-                    tzinfo=timezone.get_current_timezone()
-            ):
+        tasks = filter(lambda event: event.is_task, events)
+        tasks_all_done = not filter(lambda task: not task.is_done,tasks)
+        now = timezone.now()
+        hidden_events_count = 0
+        for event in events:
+            if tasks_all_done and getattr(event, 'is_done', False):
                 event.hidden = True
-        if getattr(event, 'hidden', False):
-            hidden_events_count += 1
-    editting = request.GET.get('editting', False)
-    context = {
-        'hide_header_footer': True,
-        'hidden_events_count': hidden_events_count,
-        'diary': diary,
-        'weekday': WEEKDAY_DICT[str(diary.date.weekday())],
-        'events': events,
-        'text_form': DiaryTextForm(),
-        'image_form': DiaryImageForm(),
-        'editting': editting,
-        'tasks_all_done': tasks_all_done,
-    }
-    context.update(base_diary_context())
-    return render(request, 'diary/diary_details.html', context)
+            if today == diary_date and event.end_hour:
+                if now > timezone.datetime(
+                        now.year, now.month, now.day,
+                        int(event.end_hour),
+                        int(event.end_min or 0),
+                        tzinfo=timezone.get_current_timezone()
+                ):
+                    event.hidden = True
+            if getattr(event, 'hidden', False):
+                hidden_events_count += 1
+        editting = request.GET.get('editting', False)
+        context = {
+            'hide_header_footer': True,
+            'hidden_events_count': hidden_events_count,
+            'diary': diary,
+            'weekday': WEEKDAY_DICT[str(diary.date.weekday())],
+            'events': events,
+            'text_form': DiaryTextForm(),
+            'image_form': DiaryImageForm(),
+            'editting': editting,
+            'tasks_all_done': tasks_all_done,
+        }
+        context.update(base_diary_context())
+        return render(request, 'diary/diary_details.html', context)
 
 
-@staff_member_required
-def diary_add_text(request, diary_id):
-    diary = Diary.objects.get(id=diary_id)
-    if request.method == "POST":
+diary_details = DiaryDetails.as_view()
+
+
+class DiaryAddText(View):
+    @method_decorator(staff_member_required)
+    def get(self, request, diary_id):
+        diary = Diary.objects.get(id=diary_id)
+        text_form = DiaryTextForm()
+        context = {
+            'hide_header_footer': True,
+            'diary': diary,
+            'text_form': text_form,
+        }
+        return render(request, 'diary/include/diary_add_text.html', context)
+
+    @method_decorator(staff_member_required)
+    def post(self, request, diary_id):
+        diary = Diary.objects.get(id=diary_id)
         text_form = DiaryTextForm(request.POST)
         if text_form.is_valid():
             diary_content = text_form.save(commit=False)
@@ -102,22 +122,32 @@ def diary_add_text(request, diary_id):
             if request.is_ajax():
                 return HttpResponse(diary_content.render())
             else:
-                diary_details_url = reverse('diary_details', args=(diary.formatted_date, ))
+                diary_details_url = reverse('diary_details', args=(diary.formatted_date,))
                 return redirect(diary_details_url)
-    else:
-        text_form = DiaryTextForm()
-    context = {
-        'hide_header_footer': True,
-        'diary': diary,
-        'text_form': text_form,
-    }
-    return render(request, 'diary/include/diary_add_text.html', context)
+        else:
+            context = {
+                'hide_header_footer': True,
+                'diary': diary,
+                'text_form': text_form,
+            }
+            return render(request, 'diary/include/diary_add_text.html', context)
 
 
-@staff_member_required
-def diary_add_image(request, diary_id):
-    diary = Diary.objects.get(id=diary_id)
-    if request.method == "POST":
+class DiaryAddImage(View):
+    @method_decorator(staff_member_required)
+    def get(self, request, diary_id):
+        diary = Diary.objects.get(id=diary_id)
+        image_form = DiaryImageForm()
+        context = {
+            'hide_header_footer': True,
+            'diary': diary,
+            'image_form': image_form,
+        }
+        return render(request, 'diary/include/diary_add_image.html', context)
+
+    @method_decorator(staff_member_required)
+    def post(self, request, diary_id):
+        diary = Diary.objects.get(id=diary_id)
         image_form = DiaryImageForm(request.POST, request.FILES)
         if image_form.is_valid():
             diary_content = image_form.save(commit=False)
@@ -127,23 +157,34 @@ def diary_add_image(request, diary_id):
             if request.is_ajax():
                 return HttpResponse(diary_content.render())
             else:
-                diary_details_url = reverse('diary_details', args=(diary.formatted_date, ))
+                diary_details_url = reverse('diary_details', args=(diary.formatted_date,))
                 return redirect(diary_details_url)
-    else:
-        image_form = DiaryImageForm()
-    context = {
-        'hide_header_footer': True,
-        'diary': diary,
-        'image_form': image_form,
-    }
-    return render(request, 'diary/include/diary_add_image.html', context)
+        context = {
+            'hide_header_footer': True,
+            'diary': diary,
+            'image_form': image_form,
+        }
+        return render(request, 'diary/include/diary_add_image.html', context)
 
 
-@staff_member_required
-def diary_edit_text(request, content_id):
-    text_content = DiaryText.objects.get(id=content_id)
-    diary = text_content.diary
-    if request.method == "POST":
+class DiaryEditText(View):
+    @method_decorator(staff_member_required)
+    def get(self, request, content_id):
+        text_content = DiaryText.objects.get(id=content_id)
+        diary = text_content.diary
+        text_form = DiaryTextForm(instance=text_content)
+        context = {
+            'hide_header_footer': True,
+            'diary': diary,
+            'text_content': text_content,
+            'text_form': text_form,
+        }
+        return render(request, 'diary/include/diary_edit_text.html', context)
+
+    @method_decorator(staff_member_required)
+    def post(self, request, content_id):
+        text_content = DiaryText.objects.get(id=content_id)
+        diary = text_content.diary
         text_form = DiaryTextForm(request.POST, instance=text_content)
         if text_form.is_valid():
             diary_content = text_form.save(commit=False)
@@ -151,92 +192,97 @@ def diary_edit_text(request, content_id):
             if request.is_ajax():
                 return HttpResponse(diary_content.render())
             else:
-                diary_details_url = reverse('diary_details', args=(diary.formatted_date, ))
+                diary_details_url = reverse('diary_details', args=(diary.formatted_date,))
                 return redirect(diary_details_url)
-    else:
-        text_form = DiaryTextForm(instance=text_content)
-    context = {
-        'hide_header_footer': True,
-        'diary': diary,
-        'text_content': text_content,
-        'text_form': text_form,
-    }
-    return render(request, 'diary/include/diary_edit_text.html', context)
+        context = {
+            'hide_header_footer': True,
+            'diary': diary,
+            'text_content': text_content,
+            'text_form': text_form,
+        }
+        return render(request, 'diary/include/diary_edit_text.html', context)
 
 
-@staff_member_required
-def diary_events(request):
-    event_types = EVENT_TYPES
-    selected_type = request.GET.get('event_type', None)
+class DiaryEvents(View):
+    @method_decorator(staff_member_required)
+    def get(self, request):
+        event_types = EVENT_TYPES
+        selected_type = request.GET.get('event_type', None)
 
-    today = date.today()
-    default_start = today
-    default_end = default_start + timedelta(days=42)
-    form = EventsRangeForm(initial={'start': default_start, 'end': default_end})
-    if request.GET:
-        form = EventsRangeForm(request.GET)
-        form.is_valid()
-        start = form.cleaned_data.get('start', None) or default_start
-        default_end = start + timedelta(days=42)
-        end = form.cleaned_data.get('end', None) or default_end
-    else:
-        start = default_start
-        end = default_end
+        today = date.today()
+        default_start = today
+        default_end = default_start + timedelta(days=42)
+        form = EventsRangeForm(initial={'start': default_start, 'end': default_end})
+        if request.GET:
+            form = EventsRangeForm(request.GET)
+            form.is_valid()
+            start = form.cleaned_data.get('start', None) or default_start
+            default_end = start + timedelta(days=42)
+            end = form.cleaned_data.get('end', None) or default_end
+        else:
+            start = default_start
+            end = default_end
 
-    shortcuts = [
-        [u'一周以前', today - timedelta(days=7),],
-        [u'一月以前', today - timedelta(days=30),],
-    ]
+        shortcuts = [
+            [u'一周以前', today - timedelta(days=7),],
+            [u'一月以前', today - timedelta(days=30),],
+        ]
 
-    current_day = start
-    days = []
-    days_and_events = []
-    while current_day < end:
-        days.append(current_day)
-        current_day += timedelta(days=1)
-    for day in days:
-        events = get_events_by_date(day)
-        if selected_type:
-            events = filter(lambda event:event.event_type==selected_type, events)
-        if events:
-            days_and_events.append({
-                'day': day,
-                'weekday': WEEKDAY_DICT[str(day.weekday())],
-                'events': events,
-            })
+        current_day = start
+        days = []
+        days_and_events = []
+        while current_day < end:
+            days.append(current_day)
+            current_day += timedelta(days=1)
+        for day in days:
+            events = get_events_by_date(day)
+            if selected_type:
+                events = filter(lambda event:event.event_type==selected_type, events)
+            if events:
+                days_and_events.append({
+                    'day': day,
+                    'weekday': WEEKDAY_DICT[str(day.weekday())],
+                    'events': events,
+                })
 
-    context = {
-        'hide_header_footer': True,
-        'days_and_events': days_and_events,
-        'event_types': event_types,
-        'selected_type': selected_type,
-        'form': form,
-        'shortcuts': shortcuts,
-    }
-    context.update(base_diary_context())
-    return render(request, 'diary/events.html', context)
-
-
-@staff_member_required
-@csrf_exempt
-def update_task(request):
-    task_id = request.POST.get('event_id', None)
-    task = Event.objects.get(id=task_id)
-    done = request.POST.get('checked', False)
-    if done.lower() == 'true':
-        task.is_done = True
-    elif done.lower() == 'false':
-        task.is_done = False
-    task.save()
-    return HttpResponse('Task Updated. ')
+        context = {
+            'hide_header_footer': True,
+            'days_and_events': days_and_events,
+            'event_types': event_types,
+            'selected_type': selected_type,
+            'form': form,
+            'shortcuts': shortcuts,
+        }
+        context.update(base_diary_context())
+        return render(request, 'diary/events.html', context)
 
 
-@staff_member_required
-def diary_todo(request):
-    todo_items = Event.objects.filter(event_date__isnull=True)
-    context = {
-        'hide_header_footer': True,
-        'events': todo_items,
-    }
-    context.update(base_diary_context())
-    return render(request, 'diary/todo.html', context)
+class UpdateTaskView(View):
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(UpdateTaskView, self).dispatch(request, *args, **kwargs)
+
+    @method_decorator(staff_member_required)
+    def post(self, request):
+        task_id = request.POST.get('event_id', None)
+        task = Event.objects.get(id=task_id)
+        done = request.POST.get('checked', False)
+        if done.lower() == 'true':
+            task.is_done = True
+        elif done.lower() == 'false':
+            task.is_done = False
+        task.save()
+        return HttpResponse('Task Updated. ')
+
+
+class DiaryTodo(View):
+    @method_decorator(staff_member_required)
+    def get(self, request):
+        todo_items = Event.objects.filter(event_date__isnull=True)
+        context = {
+            'hide_header_footer': True,
+            'events': todo_items,
+        }
+        context.update(base_diary_context())
+        return render(request, 'diary/todo.html', context)
