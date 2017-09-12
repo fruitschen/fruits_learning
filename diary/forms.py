198dbb6bbd81df23b5c081d12e78624fa2360fb5
Chen Zhe <fruitschen@gmail.com>
2017-09-12 21:50:27 +0800

    日记提示placeholder

--- diary/forms.py
+++ diary/forms.py
@@ -9,7 +9,12 @@ class DiaryTextForm(forms.ModelForm):
     class Meta:
         model = DiaryText
         fields = ['text', 'title']
-
+        widgets = {
+            'text': forms.Textarea(attrs={
+                'cols': 60, 'rows': 10,
+                'placeholder': u'早上干啥了？出门去哪里了？上课没，上课发生什么事了？玩什么玩具了吗？讲什么书了？'
+            }),
+        }
 
 class DiaryImageForm(forms.ModelForm):
 
