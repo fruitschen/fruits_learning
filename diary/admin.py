e33820f7481f7240d8e49e47c1e15ca1d241f5ac
Chen Zhe <fruitschen@gmail.com>
2017-09-12 20:30:42 +0800

    添加list_filter

--- diary/admin.py
+++ diary/admin.py
@@ -6,14 +6,20 @@ from diary.models import *
 class WeekdayEventTemplateAdmin(admin.ModelAdmin):
     model = WeekdayEventTemplate
     filter_horizontal = ['weekdays']
+    list_display = ['event', 'is_task', 'priority', ]
+    list_filter = ['is_task', 'weekdays', ]
 
 
 class MonthEventTemplateAdmin(admin.ModelAdmin):
     model = MonthEventTemplate
+    list_display = ['event', 'day', 'is_task', 'priority', ]
+    list_filter = ['is_task', 'day', ]
 
 
 class RuleEventTemplateAdmin(admin.ModelAdmin):
     model = RuleEventTemplate
+    list_display = ['event', 'is_task', 'generate_event', 'priority', 'rule']
+    list_filter = ['is_task', 'generate_event', ]
 
 
 class EventAdmin(admin.ModelAdmin):
