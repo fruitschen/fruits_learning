3f12bb5ae0aabaf519670dfeef8f9b2777a6c027
Chen Zhe <fruitschen@gmail.com>
2019-06-09 14:32:51 +0800

    project view

--- code_reading/urls.py
+++ code_reading/urls.py
@@ -4,4 +4,5 @@ from code_reading.views import *
 
 urlpatterns = [
     url(r'^$', ProjectsIndex.as_view(), name='code_reading_index'),
+    url(r'^projects/(?P<project>\w+)/$', ProjectDetails.as_view(), name='code_reading_project'),
 ]
--- code_reading/views.py
+++ code_reading/views.py
@@ -32,3 +32,25 @@ class ProjectsIndex(View):
             'projects': projects,
         }
         return render(request, 'code_reading/projects.html', context)
+
+
+class ProjectDetails(View):
+
+    @method_decorator(staff_member_required)
+    def get(self, request, project):
+        absolute_project_path = os.path.join(PROJECTS_DIR, project)
+        pwd = request.GET.get('pwd', '')
+        if not pwd:
+            absolute_pwd = absolute_project_path
+        else:
+            absolute_pwd = os.path.join(absolute_project_path, pwd)
+        files, directories = get_names(absolute_pwd)
+        directories = [{'name': d, 'pwd': os.path.join(pwd, d)} for d in directories]
+        context = {
+            'title': project,
+            'project': project,
+            'pwd': pwd,
+            'files': files,
+            'directories': directories,
+        }
+        return render(request, 'code_reading/project_details.html', context)
