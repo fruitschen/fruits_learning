e804f39ffc85aaa98c4223e8bf0bff9ce11c6072
Chen Zhe <fruitschen@gmail.com>
2021-08-18 11:44:22 +0800

    auto user account snapshot

---
+++ user_stocks/management/commands/auto_user_snapshot.py
@@ -0,0 +1,20 @@
+# -*- coding: UTF-8 -*-
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+from django.utils import timezone
+
+from user_stocks.models import UserAccount
+
+
+class Command(BaseCommand):
+    help = '每天自动创建用户账户的快照'
+
+    def handle(self, *args, **options):
+        now = timezone.now()
+        today = timezone.datetime(now.year, now.month, now.day, 15, 0, 0, tzinfo=timezone.get_current_timezone())
+        if now.hour > 15:
+            user_accounts = UserAccount.objects.all()
+            for user_account in user_accounts:
+                if not user_account.snapshots.filter(date__gte=today):
+                    call_command('get_price')
+                    user_account.take_snapshot()
